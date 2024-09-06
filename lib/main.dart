9476e7762b2e4df626cae313394c36f3c1e09014
kiu-chan <baokhanhntby@gmail.com>
2024-09-06 18:52:59 +0700

    add firebase thêm cơ sở dữ liệu firebase vào ứng dụng

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_project/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
